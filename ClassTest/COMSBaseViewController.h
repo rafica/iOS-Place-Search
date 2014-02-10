896eb156530d73b7db940914bf98cfc23ba98042
rafica <rafica.ar@gmail.com>
2014-02-09 20:11:06 -0500

    enabled dynamic pin dropping

--- ClassTest/COMSBaseViewController.h
+++ ClassTest/COMSBaseViewController.h
@@ -8,15 +8,24 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import "MapPin.h"
+
 //import the class where the CLLocationManagerDelegate protocol is implemented
 @import CoreLocation;
 
 //here between the <> we dictate what protocols we will implement. If we don't import the class that has that protocol declared, this won't work
-@interface COMSBaseViewController : UIViewController<CLLocationManagerDelegate, UITextFieldDelegate>
 
+
+@interface COMSBaseViewController : UIViewController<CLLocationManagerDelegate, UITextFieldDelegate>
+{
+    BOOL firstLaunch;
+    int currenDist;
+    CLLocationCoordinate2D currentCentre;
+}
 //connect our view controllers to the UIKit items on the storyboard
 @property (strong, nonatomic) IBOutlet UITextField *inputTextField;
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
 
 @end
--- ClassTest/COMSBaseViewController.m
+++ ClassTest/COMSBaseViewController.m
@@ -15,7 +15,6 @@
 @property (nonatomic, strong)CLLocationManager *locationManager;
 @property (nonatomic, strong) NSString *place;
 
-//@property (nonatomic,strong) NSMutableArray *nextAnnotations;
 
 @end
 
@@ -37,10 +36,10 @@
  Automatically called by the view controller
  */
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)viewDidLoad
 {
-    [super viewDidAppear:animated];
-    NSLog(@"viewwillappear");
+    [super viewDidLoad];
+    NSLog(@"viewdidload");
 
     //self.place = @"Chipotle";
     //we must subscribe as the delegate to the location manager
@@ -54,6 +53,12 @@
     
     self.mapView.showsUserLocation = YES;
 	// Do any additional setup after loading the view.
+    
+    [self.locationManager setDistanceFilter:kCLDistanceFilterNone];
+    [self.locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+
+    firstLaunch = YES;
+
 }
 
 #pragma mark - Core location
@@ -76,35 +81,19 @@
     
     [GoogleMapManager nearestVenuesForLatLong:coord withinRadius:1200 forQuery:self.place queryType:@"Restaurant+hotel" googleMapsAPIKey:@"AIzaSyDTxgToUPNt4Jm0yafQPaAXP_fiYXyMKiQ" searchCompletion:^(NSMutableArray *results) {
         
+        [self plotPositions:results];
         
         
-        
-        CLLocationCoordinate2D location;                         // coordinates of the annotation
-         // an array in which we'll save our annotations temporarily
-        MKPointAnnotation *newAnnotation;                        // the pointer to the annotation we're adding
-        
-        
-        
-        for (NSDictionary *dictionary in results)
-        {
-            // retrieve latitude and longitude from the dictionary entry
-            
-            location.latitude = [[dictionary valueForKeyPath:@"geometry.location.lat"] doubleValue];
-            location.longitude = [[dictionary valueForKeyPath:@"geometry.location.lng"] doubleValue];
-            //NSLog(@"%@",[dictionary valueForKeyPath:@"geometry.location.lat"]);
-            
-            // create the annotation
-            
-            newAnnotation = [[MKPointAnnotation alloc] init];
-            newAnnotation.title = dictionary[@"name"];
-            newAnnotation.coordinate = location;
-            NSLog(@"adding annotation");
-            [self.mapView addAnnotation:newAnnotation];
-            
-            
-        }
-            }];
+    
+    }];
+    
+    
 }
+
+
+
+
+
 #pragma mark - UITextField delegate methods
 /*
  Called by the textfield right before the text changes, great place to call a network for requests, etc... Here we'll use it to change the coor of the screen
@@ -135,22 +124,74 @@
 */
 #pragma mark - IBActions
 
+
+-(void)plotPositions:(NSArray *)results {
+    
+    // 2 - Loop through the array of places returned from the Google API.
+    for (int i=0; i<[results count]; i++) {
+        //Retrieve the NSDictionary object in each index of the array.
+        NSDictionary* place = [results objectAtIndex:i];
+        // 3 - There is a specific NSDictionary object that gives us the location info.
+        NSDictionary *geo = [place objectForKey:@"geometry"];
+        // Get the lat and long for the location.
+        NSDictionary *loc = [geo objectForKey:@"location"];
+        // 4 - Get your name and address info for adding to a pin.
+        NSString *name=[place objectForKey:@"name"];
+        NSString *vicinity=[place objectForKey:@"vicinity"];
+        // Create a special variable to hold this coordinate info.
+        CLLocationCoordinate2D placeCoord;
+        // Set the lat and long.
+        placeCoord.latitude=[[loc objectForKey:@"lat"] doubleValue];
+        placeCoord.longitude=[[loc objectForKey:@"lng"] doubleValue];
+        // 5 - Create a new annotation.
+        MapPin *placeObject = [[MapPin alloc] initWithName:name address:vicinity coordinate:placeCoord];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.mapView addAnnotation:placeObject];
+        });
+        
+    }
+    
+    
+    
+    
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation {
+    
+    //Define our reuse indentifier.
+    static NSString *identifier = @"MapPoint";
+    
+    
+    if ([annotation isKindOfClass:[MapPin class]]) {
+        
+        MKPinAnnotationView *annotationView = (MKPinAnnotationView *) [self.mapView dequeueReusableAnnotationViewWithIdentifier:identifier];
+        if (annotationView == nil) {
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:identifier];
+        } else {
+            annotationView.annotation = annotation;
+        }
+        
+        annotationView.enabled = YES;
+        annotationView.canShowCallout = YES;
+        annotationView.animatesDrop = YES;
+        
+        return annotationView;
+    }
+    
+    return nil;
+}
+
+
 /*
  Actions when the main (only) button is pressed
  */
 
-- (IBAction)updateLabelPressed:(UIButton *)sender {
+-(IBAction)updateLabelPressed:(UIButton *)sender {
     
     //set the display label text to that of the textfield
     self.place = self.inputTextField.text;
-    NSLog(@"updateLabel-removing annotations");
-    
-    NSInteger toRemoveCount = self.mapView.annotations.count;
-    NSMutableArray *toRemove = [NSMutableArray arrayWithCapacity:toRemoveCount];
-    for (id annotation in self.mapView.annotations)
-        if (annotation != self.mapView.userLocation)
-            [toRemove addObject:annotation];
-    [self.mapView removeAnnotations:toRemove];
+   
+    [self removeMapViewAnnotations];
     
     
         //resign first responder (hide the keyboard)
@@ -159,10 +200,66 @@
     }
 }
 
+-(void)removeMapViewAnnotations{
+    
+    NSLog(@"updateLabel-removing annotations");
+    
+    NSInteger toRemoveCount = self.mapView.annotations.count;
+    NSMutableArray *toRemove = [NSMutableArray arrayWithCapacity:toRemoveCount];
+    for (id annotation in self.mapView.annotations)
+        if (annotation != self.mapView.userLocation)
+            [toRemove addObject:annotation];
+    [self.mapView removeAnnotations:toRemove];
+}
+
+/*
+- (void)mapView:(MKMapView *)mv didAddAnnotationViews:(NSArray *)views
+{
+    NSLog(@"didannotationviews");
+    
+    //Zoom back to the user location after adding a new set of annotations.
+    
+    //Get the center point of the visible map.
+    CLLocationCoordinate2D centre = [mv centerCoordinate];
+    
+    MKCoordinateRegion region;
+    
+    //If this is the first launch of the app then set the center point of the map to the user's location.
+    if (firstLaunch) {
+        region = MKCoordinateRegionMakeWithDistance(self.locationManager.location.coordinate,1000,1000);
+        firstLaunch=NO;
+    }else {
+        //Set the center point to the visible region of the map and change the radius to match the search radius passed to the Google query string.
+        region = MKCoordinateRegionMakeWithDistance(centre,currenDist,currenDist);
+    }
+    
+    //Set the visible region of the map.
+    [mv setRegion:region animated:YES];
+    
+}*/
+/*
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+    
+    //Get the east and west points on the map so we calculate the distance (zoom level) of the current map view.
+    MKMapRect mRect = self.mapView.visibleMapRect;
+    MKMapPoint eastMapPoint = MKMapPointMake(MKMapRectGetMinX(mRect), MKMapRectGetMidY(mRect));
+    MKMapPoint westMapPoint = MKMapPointMake(MKMapRectGetMaxX(mRect), MKMapRectGetMidY(mRect));
+    
+    //Set our current distance instance variable.
+    currenDist = MKMetersBetweenMapPoints(eastMapPoint, westMapPoint);
+    
+    //Set our current centre point on the map instance variable.
+    currentCentre = self.mapView.centerCoordinate;
+}
+*/
+
+
 #pragma mark - Gesture recognizers
 /*
  Actions when a certain view is tapped. In this case we added this recognizer to the main view (self.view) so that the keyboard will dismiss when the screen is tapped
  */
+     
+
 - (IBAction)screenTapped:(UITapGestureRecognizer *)sender {
     NSLog(@"screentapped");
     //The textfield has a property called first responder. The keyboard is this textfields accessory view. When the textfield is in first responder mode, it means it is the primary focus on the screen (the textfield cursor is blinking, keyboard is up). To dismiss the keyboard, we tell it to resignFirstResponder status
@@ -172,11 +269,12 @@
 }
 
 
+
+
+
+
+
 @end
-
-
-
-
 
 
 
